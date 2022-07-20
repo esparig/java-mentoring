ddd76769bd012f747428d48605681948a86d473c
javi <javi@localhost>
2022-07-20 17:30:11 +0200

    Solution

--- Table.java
+++ Table.java
@@ -1,4 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Table {
+public class Table{
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        int i;
+        
+        for (i= 1; i < 11; i++) {
+			System.out.println(n + " x " + i + " = " + (n*i));
+		}
+
+        bufferedReader.close();
+    }
 }
