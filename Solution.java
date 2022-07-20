865473647e170e7dbbfb521002c18905f360dc7e
javi <javi@localhost>
2022-07-20 16:52:23 +0200

    Formatting Strings Solution

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution {
+
+	    public static void main(String[] args) {
+	            Scanner sc = new Scanner(System.in);
+	            System.out.println("================================");
+	            for (int i = 0; i < 3; i++) {
+	                String inputString = sc.next();
+	                int intputInt = sc.nextInt();
+	                // Complete this line
+	                System.out.println(inputString + "\t\t" + intputInt);
+	                
+	            }
+	            
+	            System.out.println("================================");
+	            
+	    }
+	}
+
+
